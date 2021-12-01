1e9540d87b07054515a46d0b9c77a03047edeeb2
Martin Klewitz <60610432+mklewitz@users.noreply.github.com>
2021-12-01 09:12:58 +0100

    @angular/cli only

--- .ng-dev/config.ts
+++
@@ -1,5 +0,0 @@
-export { commitMessage } from './commit-message';
-export { format } from './format';
-export { github } from './github';
-export { pullRequest } from './pull-request';
-export { release } from './release';
